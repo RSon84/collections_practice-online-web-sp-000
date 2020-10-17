233b5f461227a7a2a40f730347a5787f9c8d9068
Devin Wilson <craigvin84@yahoo.com>
2020-10-17 23:19:57 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,28 @@
 
+def sort_array_asc(array)
+  array.collect.sort do |a, b|
+    a <=> b
+  end
+end
+
+def sort_array_desc(array)
+  array.collect.sort do |a, b|
+    b <=> a
+  end
+end
+
+
+def sort_array_char_count(array)
+  array.collect.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+array.reverse
+end
